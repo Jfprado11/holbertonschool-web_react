f6f8e73ddf1e83f7e3953b76216cc39a4526a7d2
Felipe Prado <jfpc11@misena.edu.co>
2022-05-12 18:09:53 -0500

    adding the delay

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-
-$('<button>Click here to get started</button>').appendTo('body');
-$('<p id="count"></p>').appendTo('body');
-$('<p>Copyright - Holberton School</p>').appendTo('body');
-
 function updateCounter() {
   let current = 0;
   $('button').on(

12e212910254d86be7493b98c0332833614156ca
Felipe Prado <jfpc11@misena.edu.co>
2022-05-12 16:06:55 -0500

    loking for errors withou doing anything

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,1 @@
 import $ from 'jquery';
-
-$('<p>Holberton Dashboard</p>').appendTo(document.body);
-$('<p>Dashboard data for the students</p>').appendTo(document.body);
-$('<p>Copyright - Holberton School</p>').appendTo(document.body);

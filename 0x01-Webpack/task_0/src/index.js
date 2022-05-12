7989cc620457bc1278941d3f0e906c810969b9bd
Felipe Prado <jfpc11@misena.edu.co>
2022-05-12 16:02:22 -0500

    adding some files

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$('<p>Holberton Dashboard</p>').appendTo(document.body);
+$('<p>Dashboard data for the students</p>').appendTo(document.body);
+$('<p>Copyright - Holberton School</p>').appendTo(document.body);

e82e6897d221dc6a2d426b8dee050ef80bed786b
Felipe Prado <jfpc11@misena.edu.co>
2022-05-18 22:39:06 -0500

    setting the tests

---
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './Holberton_Logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="input-email">Email: </label>
+        <input id="input-email" type="email" />
+        <label htmlFor="input-password">Password: </label>
+        <input id="input-password" type="password" />
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(false)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
+
+function Notifications() {
+  const styles = {
+    position: 'absolute',
+    right: '5px',
+    top: '5px',
+    backgroundColor: 'white',
+    border: 'none',
+  };
+  return (
+    <div className="Notifications">
+      <button
+        style={styles}
+        aria-label="Close"
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img src={closeIcon} alt="close icon" width="10" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;

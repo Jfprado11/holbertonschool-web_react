c1c1ebf3a988d3dd9a6c32706a7258cfd11a6d88
Felipe Prado <jfpc11@misena.edu.co>
2022-05-23 10:53:11 -0500

    adding the config

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,29 @@
+/** @type {import('webpack').Configuration} */
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      { test: /.\css$/, use: ['css-loader', 'style-loader'] },
+      {
+        test: /.\(png|jpg)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: { bypassOnDebug: true, disable: true },
+          },
+        ],
+      },
+    ],
+  },
+};

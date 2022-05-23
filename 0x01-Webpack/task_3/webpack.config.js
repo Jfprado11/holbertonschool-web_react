c854cc036ef3e73636eb743140f2e2d2f2e469f7
Felipe Prado <jfpc11@misena.edu.co>
2022-05-23 11:32:07 -0500

    adding the errors

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,40 @@
+/** @type {import('webpack').Configuration} */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['css-loader', 'style-loader'] },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: { bypassOnDebug: true, disable: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  devserver: {
+    contentBase: './dist',
+  },
+};

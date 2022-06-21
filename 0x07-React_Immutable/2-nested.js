a48120d369cf7c39b0e12258b29f8f506a5d9a58
Felipe Prado <jfpc11@misena.edu.co>
2022-06-21 10:43:17 -0500

    nesting and obj with getIn

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,12 +1,7 @@
 import { Map } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  if (array.length <= 0) {
-    return Map(object);
-  }
-  const value = array.reduce((prev, current, idx) => {
-    if (idx === 0) return object[current];
-    return prev[current];
-  }, {});
-  return value;
+  const deepMap = Map(object);
+  const data = deepMap.getIn(array);
+  return data;
 }

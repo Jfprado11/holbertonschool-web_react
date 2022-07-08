bb253043272a10115e47588215f452b9bda6216e
Felipe Prado <jfpc11@misena.edu.co>
2022-07-08 17:37:27 -0500

    bounding the actions to the dispatch

---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,39 @@
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+} from './uiActionTypes';
+
+function login(email, password) {
+  return {
+    type: LOGIN,
+    user: {
+      email,
+      password,
+    },
+  };
+}
+export const boundLogin = (email, password) => dispatch(login(email, password));
+
+function logout() {
+  return { type: LOGOUT };
+}
+
+export const boundLogout = () => dispatch(logout());
+
+function displayNotificationDrawer() {
+  return { type: DISPLAY_NOTIFICATION_DRAWER };
+}
+
+export const boundDisplayNotificationDrawer = () =>
+  dispatch(displayNotificationDrawer());
+
+function hideNotificationDrawer() {
+  return { type: HIDE_NOTIFICATION_DRAWER };
+}
+
+export const boundHideNotificationDrawer = () =>
+  dispatch(hideNotificationDrawer());
+
+export { login, logout, displayNotificationDrawer, hideNotificationDrawer };
